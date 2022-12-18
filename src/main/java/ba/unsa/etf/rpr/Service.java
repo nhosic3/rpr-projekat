794931728170a1feea7afbad5948ea6570d562d5
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-18 20:11:32 +0100

    Initial commit

--- src/main/java/ba/unsa/etf/rpr/Service.java
+++ src/main/java/ba/unsa/etf/rpr/Service.java
@@ -9,4 +9,13 @@ public class Service {
     private Date start_time;
     private Date end_time;
     private String client_id;
+
+    public Service(String service_id, String service_type, int price, Date start_time, Date end_time, String client_id) {
+        this.service_id = service_id;
+        this.service_type = service_type;
+        this.price = price;
+        this.start_time = start_time;
+        this.end_time = end_time;
+        this.client_id = client_id;
+    }
 }
