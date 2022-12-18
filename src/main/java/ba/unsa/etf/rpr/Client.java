ef83ef2cdae2b5696a9aa0a51365626fd7a376c0
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-18 19:59:34 +0100

    updated vol2

---
+++ src/main/java/ba/unsa/etf/rpr/Client.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Date;
+
+public class Client {
+    private String client_id;
+    private String first_name, last_name;
+    private String phone_number;
+    private String email;
+    private boolean paid;
+}
