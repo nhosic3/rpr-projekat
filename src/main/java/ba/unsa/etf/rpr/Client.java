9de2069671dc05b7c6e57afbc334877ac87fc249
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-18 20:06:31 +0100

    Initial commit

--- src/main/java/ba/unsa/etf/rpr/Client.java
+++ src/main/java/ba/unsa/etf/rpr/Client.java
@@ -17,4 +17,52 @@ public class Client {
         this.email = email;
         this.paid = paid;
     }
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
 }
