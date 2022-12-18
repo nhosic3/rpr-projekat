62a1f8073109e8a8973f299f56c10343d080717c
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-18 20:12:00 +0100

    Initial commit

--- src/main/java/ba/unsa/etf/rpr/Service.java
+++ src/main/java/ba/unsa/etf/rpr/Service.java
@@ -18,4 +18,52 @@ public class Service {
         this.end_time = end_time;
         this.client_id = client_id;
     }
+
+    public String getService_id() {
+        return service_id;
+    }
+
+    public void setService_id(String service_id) {
+        this.service_id = service_id;
+    }
+
+    public String getService_type() {
+        return service_type;
+    }
+
+    public void setService_type(String service_type) {
+        this.service_type = service_type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(Date start_time) {
+        this.start_time = start_time;
+    }
+
+    public Date getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(Date end_time) {
+        this.end_time = end_time;
+    }
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
 }
