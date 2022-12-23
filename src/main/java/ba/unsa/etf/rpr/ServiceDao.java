a61dda08e80694a2a25115774893f87adbedb269
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-23 17:47:41 +0100

    ispravka

--- src/main/java/ba/unsa/etf/rpr/ServiceDao.java
+++ src/main/java/ba/unsa/etf/rpr/ServiceDao.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr;
 
-public interface ServiceDao {
+import java.util.List;
+
+public interface ServiceDao extends Dao<Service> {
+    /**
+     * Searches services by service type
+     */
+    List<Service> searchByServiceType(String serviceType);
+
+    /**
+     * Searches services by ID
+     */
+    List<Service> searchById(int id);
 }
