adb4b6b6aeb24302530f4b6757b0e7b32e0fca22
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-23 17:40:44 +0100

    ispravka

--- src/main/java/ba/unsa/etf/rpr/Employee.java
+++ src/main/java/ba/unsa/etf/rpr/Employee.java
@@ -4,15 +4,15 @@ import java.util.Date;
 import java.util.Objects;
 
 public class Employee {
-    private String employee_id;
+    private int id;
     private String first_name, last_name;
     private Date brith_date;
     private Date hire_date;
     private int salary;
-    private String service_id;
+    private int service_id;
 
-    public Employee(String employee_id, String first_name, String last_name, Date brith_date, Date hire_date, int salary, String service_id) {
-        this.employee_id = employee_id;
+    public Employee(int id, String first_name, String last_name, Date brith_date, Date hire_date, int salary, int service_id) {
+        this.id = id;
         this.first_name = first_name;
         this.last_name = last_name;
         this.brith_date = brith_date;
@@ -21,12 +21,12 @@ public class Employee {
         this.service_id = service_id;
     }
 
-    public String getEmployee_id() {
-        return employee_id;
+    public int getId() {
+        return id;
     }
 
-    public void setEmployee_id(String employee_id) {
-        this.employee_id = employee_id;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getFirst_name() {
@@ -69,11 +69,11 @@ public class Employee {
         this.salary = salary;
     }
 
-    public String getService_id() {
+    public int getService_id() {
         return service_id;
     }
 
-    public void setService_id(String service_id) {
+    public void setService_id(int service_id) {
         this.service_id = service_id;
     }
 
@@ -82,24 +82,24 @@ public class Employee {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
-        return salary == employee.salary && Objects.equals(employee_id, employee.employee_id) && Objects.equals(first_name, employee.first_name) && Objects.equals(last_name, employee.last_name) && Objects.equals(brith_date, employee.brith_date) && Objects.equals(hire_date, employee.hire_date) && Objects.equals(service_id, employee.service_id);
+        return id == employee.id && salary == employee.salary && service_id == employee.service_id && Objects.equals(first_name, employee.first_name) && Objects.equals(last_name, employee.last_name) && Objects.equals(brith_date, employee.brith_date) && Objects.equals(hire_date, employee.hire_date);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(employee_id, first_name, last_name, brith_date, hire_date, salary, service_id);
+        return Objects.hash(id, first_name, last_name, brith_date, hire_date, salary, service_id);
     }
 
     @Override
     public String toString() {
         return "Employee{" +
-                "employee_id='" + employee_id + '\'' +
+                "id=" + id +
                 ", first_name='" + first_name + '\'' +
                 ", last_name='" + last_name + '\'' +
                 ", brith_date=" + brith_date +
                 ", hire_date=" + hire_date +
                 ", salary=" + salary +
-                ", service_id='" + service_id + '\'' +
+                ", service_id=" + service_id +
                 '}';
     }
 }
