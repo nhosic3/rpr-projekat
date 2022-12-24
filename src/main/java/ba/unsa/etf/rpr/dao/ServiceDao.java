266b4970b379eac99c156ea46b367bf1964e3e0a
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-24 15:55:40 +0100

    ServiceDao updated

--- src/main/java/ba/unsa/etf/rpr/dao/ServiceDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ServiceDao.java
@@ -1,17 +1,29 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Service;
+import ba.unsa.etf.rpr.exceptions.ServiceException;
 
+import java.util.Date;
 import java.util.List;
 
 public interface ServiceDao extends Dao<Service> {
     /**
      * Searches services by service type
      */
-    List<Service> searchByServiceType(String serviceType);
+    List<Service> searchByServiceType(String serviceType) throws ServiceException;
 
     /**
-     * Searches services by ID
+     * Searches services by Client ID
      */
-    List<Service> searchById(int id);
+    List<Service> searchByClientId(int id) throws ServiceException;
+
+    /**
+     * Searches services by start time
+     */
+    List<Service> searchByStartTime(Date st) throws ServiceException;
+
+    /**
+     * Searches services by end time
+     */
+    List<Service> searchByEndTime(Date et) throws ServiceException;
 }
