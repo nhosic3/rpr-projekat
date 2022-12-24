2a2e3c11c6bebf5ae125f5fd5d4fa561f86f1cb8
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-24 22:10:57 +0100

    Client updated

--- src/main/java/ba/unsa/etf/rpr/domain/Client.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Client.java
@@ -6,7 +6,7 @@ import java.util.Objects;
  * Client data
  */
 public class Client implements Idable {
-    private int id;
+    private int ID;
     private String first_name, last_name;
     private String phone_number;
     private String email;
@@ -15,8 +15,8 @@ public class Client implements Idable {
     public Client(){
 
     }
-    public Client(int id, String first_name, String last_name, String phone_number, String email, boolean paid) {
-        this.id = id;
+    public Client(int ID, String first_name, String last_name, String phone_number, String email, boolean paid) {
+        this.ID = ID;
         this.first_name = first_name;
         this.last_name = last_name;
         this.phone_number = phone_number;
@@ -25,13 +25,13 @@ public class Client implements Idable {
     }
 
     @Override
-    public int getId() {
-        return id;
+    public int getID() {
+        return ID;
     }
 
     @Override
-    public void setId(int id) {
-        this.id = id;
+    public void setID(int ID) {
+        this.ID = ID;
     }
 
     public String getFirst_name() {
@@ -79,18 +79,18 @@ public class Client implements Idable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Client client = (Client) o;
-        return id == client.id && paid == client.paid && Objects.equals(first_name, client.first_name) && Objects.equals(last_name, client.last_name) && Objects.equals(phone_number, client.phone_number) && Objects.equals(email, client.email);
+        return ID == client.ID && paid == client.paid && Objects.equals(first_name, client.first_name) && Objects.equals(last_name, client.last_name) && Objects.equals(phone_number, client.phone_number) && Objects.equals(email, client.email);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, first_name, last_name, phone_number, email, paid);
+        return Objects.hash(ID, first_name, last_name, phone_number, email, paid);
     }
 
     @Override
     public String toString() {
         return "Client{" +
-                "id=" + id +
+                "id=" + ID +
                 ", first_name='" + first_name + '\'' +
                 ", last_name='" + last_name + '\'' +
                 ", phone_number='" + phone_number + '\'' +
