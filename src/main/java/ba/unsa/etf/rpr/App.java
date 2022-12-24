ed14d223b31a3dd937ef13f3d962b240a787fb19
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-24 22:11:15 +0100

    App updated

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,7 +1,7 @@
 package ba.unsa.etf.rpr;
 
-import ba.unsa.etf.rpr.dao.*;
 import ba.unsa.etf.rpr.domain.Client;
+import ba.unsa.etf.rpr.dao.ClientDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Service;
 import ba.unsa.etf.rpr.domain.Employee;
 import ba.unsa.etf.rpr.exceptions.ServiceException;
@@ -16,8 +16,19 @@ import java.util.*;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        try{
+            ClientDaoSQLImpl c = new ClientDaoSQLImpl();
+            Client cc = new Client();
+            cc.setPhone_number("12345679");
+            cc.setPaid(true);
+            //cc.setID(1324);
+            cc.setEmail("asdasd@gmail.com");
+            cc.setFirst_name("Nedim");
+            cc.setLast_name("Hosic");
+            c.add(cc);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
