a45a652eb6dd0f7ee5a6c546c037431bcad14a98
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-24 16:23:00 +0100

    DaoFactory updated

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,4 +1,23 @@
 package ba.unsa.etf.rpr.dao;
 
 public class DaoFactory {
+    private static final ClientDao clientDao = new ClientDaoSQLImpl();
+    private static final EmployeeDao employeeDao = new EmployeeDaoSQLImpl();
+    private static final ServiceDao serviceDao = new ServiceDaoSQLImpl();
+
+    private DaoFactory(){
+
+    }
+
+    public static ClientDao clientDao(){
+        return clientDao;
+    }
+
+    public static EmployeeDao employeeDao(){
+        return employeeDao;
+    }
+
+    public static ServiceDao serviceDao(){
+        return serviceDao;
+    }
 }
