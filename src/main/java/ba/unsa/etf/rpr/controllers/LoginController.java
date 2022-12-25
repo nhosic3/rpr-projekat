400d2e60e18914c816439dd93754256b8f2a801c
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-25 21:24:58 +0100

    login controller added

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    public TextField emailId;
+    public PasswordField passwordId;
+    public Button loginButtonId;
+
+    public void loginAction(ActionEvent actionEvent) {
+    }
+}
