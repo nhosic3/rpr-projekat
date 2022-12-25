4b5c6c84d39a89455d39eb87215bb6f8d4480c23
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-25 23:14:11 +0100

    HomeController updated

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class HomeController {
+    public Label welcomeField;
+
+    public void closeAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) welcomeField.getScene().getWindow();
+        stage.close();
+    }
 }
