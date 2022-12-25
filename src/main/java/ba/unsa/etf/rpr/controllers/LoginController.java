d48c71b01753ede581361de147e943a7daffa4ad
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-25 21:52:54 +0100

    LoginController updated

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -4,12 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
 public class LoginController {
     public TextField emailId;
     public PasswordField passwordId;
     public Button loginButtonId;
 
     public void loginAction(ActionEvent actionEvent) {
+
     }
 }
