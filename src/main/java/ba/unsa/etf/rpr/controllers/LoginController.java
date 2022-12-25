4ae5fb2d546771457a7e28aa7ed7420a9f3420d8
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-25 22:31:50 +0100

    LoginController updated

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,6 +1,9 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -8,6 +11,29 @@ public class LoginController {
     public TextField emailId;
     public PasswordField passwordId;
     public Button loginButtonId;
+
+    @FXML
+    public void initialize() {
+        emailId.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if(emailId.getText().isEmpty()){
+                    emailId.getStyleClass().add("incorrectField");
+                }
+            }
+        });
+        passwordId.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if(passwordId.getText().length()<8){
+                    passwordId.getStyleClass().add("incorrectField");
+                }
+                else{
+                    passwordId.getStyleClass().add("correctField");
+                }
+            }
+        });
+    }
 
     public void loginAction(ActionEvent actionEvent) {
 
