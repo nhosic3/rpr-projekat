5f36774754863f307ec8a05e280ae03d45c7fd8e
nhosic3 <nhosic3@etf.unsa.ba>
2022-12-26 15:44:45 +0100

    ClientDao updated

--- src/main/java/ba/unsa/etf/rpr/dao/ClientDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ClientDao.java
@@ -10,4 +10,9 @@ public interface ClientDao extends Dao<Client> {
      * Searches clients by names
      */
     List<Client> searchByName(String name) throws ServiceException;
+
+    /**
+     * Searches clients by names
+     */
+    Client searchByEmail(String email) throws ServiceException;
 }
