dd97548a1f5663b8b7d5ab697b28e0f26a9f0dda
nhosic3 <nhosic3@etf.unsa.ba>
2023-02-11 22:38:27 +0100

    HomeController updated

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,9 +1,16 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class HomeController {
     public Label welcomeField;
@@ -11,5 +18,16 @@ public class HomeController {
     public void closeAction(ActionEvent actionEvent) {
         Stage stage = (Stage) welcomeField.getScene().getWindow();
         stage.close();
+    }
+
+    public void buyAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader l = new FXMLLoader(getClass().getResource("/FXML/buy.fxml"));
+        Parent root = l.load();
+        BuyController rc = l.getController();
+        stage.setResizable(false);
+        stage.setTitle("Buy");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.show();
     }
 }
