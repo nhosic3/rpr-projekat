62a9282078c7f821d357c04a0a35e0a3a32c0392
nhosic3 <nhosic3@etf.unsa.ba>
2023-02-12 13:19:44 +0100

    BuyController updated

--- src/main/java/ba/unsa/etf/rpr/controllers/BuyController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/BuyController.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.ClientDaoSQLImpl;
 import ba.unsa.etf.rpr.dao.ServiceDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Client;
 import ba.unsa.etf.rpr.domain.Service;
 import javafx.fxml.FXML;
 import ba.unsa.etf.rpr.exceptions.ServiceException;
@@ -8,10 +10,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class BuyController {
     public ChoiceBox<String> cb;
+    public TextField fn;
+    public TextField ln;
+    public TextField email;
+
     @FXML
     public void initialize() {
         ObservableList<String> l = FXCollections.observableArrayList();
@@ -23,7 +34,19 @@ public class BuyController {
         cb.getItems().addAll(l);
     }
     public void purchaseAction(ActionEvent actionEvent) throws ServiceException {
-
+        ClientDaoSQLImpl c = new ClientDaoSQLImpl();
+        Client cl = c.searchByEmail(email.getText());
+        LocalDate l = LocalDate.now();
+        ZoneId defaultZoneId = ZoneId.systemDefault();
+        Date date = Date.from(l.atStartOfDay(defaultZoneId).toInstant());
+        LocalDate l2 =  LocalDate.now().plusDays(15);
+        Date date2 = Date.from(l2.atStartOfDay(defaultZoneId).toInstant());
+        ServiceDaoSQLImpl s = new ServiceDaoSQLImpl();
+        Service ser = new Service(1, cb.getValue(), 123, date, date2, cl.getID());
+        s.add(ser);
+        cl.setPaid(true);
+        Stage stage = (Stage) cb.getScene().getWindow();
+        stage.close();
     }
 
     public void cancelAction(ActionEvent actionEvent) {
