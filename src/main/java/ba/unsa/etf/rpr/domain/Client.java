a2abe713a8d8d555f695313b384ae13f374c68a0
nhosic3 <nhosic3@etf.unsa.ba>
2023-02-12 22:24:00 +0100

    Client updated

--- src/main/java/ba/unsa/etf/rpr/domain/Client.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Client.java
@@ -11,19 +11,17 @@ public class Client implements Idable {
     private String phone_number;
     private String email;
     private String password;
-    private boolean paid;
 
     public Client(){
 
     }
-    public Client(int ID, String first_name, String last_name, String phone_number, String email, String password, boolean paid) {
+    public Client(int ID, String first_name, String last_name, String phone_number, String email, String password) {
         this.ID = ID;
         this.first_name = first_name;
         this.last_name = last_name;
         this.phone_number = phone_number;
         this.email = email;
         this.password = password;
-        this.paid = paid;
     }
 
     @Override
@@ -76,25 +74,17 @@ public class Client implements Idable {
         this.password = password;
     }
 
-    public boolean isPaid() {
-        return paid;
-    }
-
-    public void setPaid(boolean paid) {
-        this.paid = paid;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Client client = (Client) o;
-        return ID == client.ID && paid == client.paid && Objects.equals(first_name, client.first_name) && Objects.equals(last_name, client.last_name) && Objects.equals(phone_number, client.phone_number) && Objects.equals(email, client.email);
+        return ID == client.ID && Objects.equals(first_name, client.first_name) && Objects.equals(last_name, client.last_name) && Objects.equals(phone_number, client.phone_number) && Objects.equals(email, client.email);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(ID, first_name, last_name, phone_number, email, paid);
+        return Objects.hash(ID, first_name, last_name, phone_number, email);
     }
 
     @Override
@@ -105,7 +95,6 @@ public class Client implements Idable {
                 ", last_name='" + last_name + '\'' +
                 ", phone_number='" + phone_number + '\'' +
                 ", email='" + email + '\'' +
-                ", paid=" + paid +
                 '}';
     }
 }
