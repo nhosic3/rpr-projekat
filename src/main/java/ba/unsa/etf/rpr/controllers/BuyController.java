056657616f37efc2ac62af2b3a29d070a99abbc6
nhosic3 <nhosic3@etf.unsa.ba>
2023-02-12 12:38:58 +0100

    BuyController updated

--- src/main/java/ba/unsa/etf/rpr/controllers/BuyController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/BuyController.java
@@ -1,4 +1,33 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.ServiceDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Service;
+import javafx.fxml.FXML;
+import ba.unsa.etf.rpr.exceptions.ServiceException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ChoiceBox;
+import javafx.stage.Stage;
+
 public class BuyController {
+    public ChoiceBox<String> cb;
+    @FXML
+    public void initialize() {
+        ObservableList<String> l = FXCollections.observableArrayList();
+        l.add("Angular");
+        l.add("HTML/CSS/JS");
+        l.add("Node");
+        l.add("Photoshop");
+        l.add("React");
+        cb.getItems().addAll(l);
+    }
+    public void purchaseAction(ActionEvent actionEvent) throws ServiceException {
+
+    }
+
+    public void cancelAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) cb.getScene().getWindow();
+        stage.close();
+    }
 }
