68e1525df3a4eeacc564b5a15eb509b02091a323
nhosic3 <nhosic3@etf.unsa.ba>
2023-02-12 15:32:21 +0100

    BuyController updated

--- src/main/java/ba/unsa/etf/rpr/controllers/BuyController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/BuyController.java
@@ -16,12 +16,15 @@ import javafx.stage.Stage;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BuyController {
     public ChoiceBox<String> cb;
     public TextField fn;
     public TextField ln;
     public TextField email;
+    public int p;
 
     @FXML
     public void initialize() {
@@ -34,6 +37,13 @@ public class BuyController {
         cb.getItems().addAll(l);
     }
     public void purchaseAction(ActionEvent actionEvent) throws ServiceException {
+        Map<String, Integer> price = new HashMap<String, Integer>();
+        price.put("Angular", 100);
+        price.put("HTML/CSS/JS", 70);
+        price.put("Node", 150);
+        price.put("Photoshop", 30);
+        price.put("React", 200);
+
         ClientDaoSQLImpl c = new ClientDaoSQLImpl();
         Client cl = c.searchByEmail(email.getText());
         LocalDate l = LocalDate.now();
@@ -42,7 +52,7 @@ public class BuyController {
         LocalDate l2 =  LocalDate.now().plusDays(15);
         Date date2 = Date.from(l2.atStartOfDay(defaultZoneId).toInstant());
         ServiceDaoSQLImpl s = new ServiceDaoSQLImpl();
-        Service ser = new Service(1, cb.getValue(), 123, date, date2, cl.getID());
+        Service ser = new Service(1, cb.getValue(),getPrice(price, cb.getValue()) , date, date2, cl.getID());
         s.add(ser);
         cl.setPaid(true);
         Stage stage = (Stage) cb.getScene().getWindow();
@@ -52,5 +62,12 @@ public class BuyController {
     public void cancelAction(ActionEvent actionEvent) {
         Stage stage = (Stage) cb.getScene().getWindow();
         stage.close();
+    }
+
+    public int getPrice(Map<String, Integer> m, String s){
+        for (Map.Entry<String, Integer> e : m.entrySet()){
+            if(e.getKey() == s) return e.getValue();
+        }
+        return 0;
     }
 }
