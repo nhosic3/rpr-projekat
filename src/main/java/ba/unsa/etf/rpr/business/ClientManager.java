dd8bf0cbae181d50c735a822c4b1970c70f6e9d3
nhosic3 <nhosic3@etf.unsa.ba>
2023-02-12 14:13:46 +0100

    ClientManager updated

--- src/main/java/ba/unsa/etf/rpr/business/ClientManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ClientManager.java
@@ -1,4 +1,32 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Client;
+import ba.unsa.etf.rpr.exceptions.ServiceException;
+
+import java.util.List;
+
 public class ClientManager {
+    public List<Client> searchByName(String n) throws ServiceException {
+        return DaoFactory.clientDao().searchByName(n);
+    }
+
+    public Client searchByEmail(String e) throws ServiceException {
+        return DaoFactory.clientDao().searchByEmail(e);
+    }
+    public Client add(Client c) throws ServiceException {
+        return DaoFactory.clientDao().add(c);
+    }
+
+    public List<Client> getAll() throws ServiceException {
+        return DaoFactory.clientDao().getAll();
+    }
+
+    public Client update(Client c) throws ServiceException {
+        return DaoFactory.clientDao().update(c);
+    }
+
+    public void delete(int id) throws ServiceException {
+        DaoFactory.clientDao().delete(id);
+    }
 }
