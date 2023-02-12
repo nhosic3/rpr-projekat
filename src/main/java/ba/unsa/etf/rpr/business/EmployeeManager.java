ee1d87580aa90f4eac36cf0c582f092eb28fb19f
nhosic3 <nhosic3@etf.unsa.ba>
2023-02-12 14:18:21 +0100

    EmployeeManager updated

--- src/main/java/ba/unsa/etf/rpr/business/EmployeeManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/EmployeeManager.java
@@ -1,4 +1,33 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Client;
+import ba.unsa.etf.rpr.domain.Employee;
+import ba.unsa.etf.rpr.exceptions.ServiceException;
+
+import java.util.List;
+
 public class EmployeeManager {
+    public List<Employee> searchByName(String n) throws ServiceException {
+        return DaoFactory.employeeDao().searchByName(n);
+    }
+
+    public List<Employee> searchByServiceId(int s_id) throws ServiceException {
+        return DaoFactory.employeeDao().searchByServiceId(s_id);
+    }
+    public Employee add(Employee e) throws ServiceException {
+        return DaoFactory.employeeDao().add(e);
+    }
+
+    public List<Employee> getAll() throws ServiceException {
+        return DaoFactory.employeeDao().getAll();
+    }
+
+    public Employee update(Employee e) throws ServiceException {
+        return DaoFactory.employeeDao().update(e);
+    }
+
+    public void delete(int id) throws ServiceException {
+        DaoFactory.employeeDao().delete(id);
+    }
 }
