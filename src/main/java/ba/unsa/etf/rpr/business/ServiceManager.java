fe5000f6cde06784711908d0a2c28d94c891520d
nhosic3 <nhosic3@etf.unsa.ba>
2023-02-12 14:20:52 +0100

    ServiceManager updated

--- src/main/java/ba/unsa/etf/rpr/business/ServiceManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ServiceManager.java
@@ -1,4 +1,33 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Client;
+import ba.unsa.etf.rpr.domain.Service;
+import ba.unsa.etf.rpr.exceptions.ServiceException;
+
+import java.util.List;
+
 public class ServiceManager {
+    public List<Service> searchByServiceType(String st) throws ServiceException {
+        return DaoFactory.serviceDao().searchByServiceType(st);
+    }
+
+    public List<Service> searchByClientId(int c_id) throws ServiceException {
+        return DaoFactory.serviceDao().searchByClientId(c_id);
+    }
+    public Service add(Service s) throws ServiceException {
+        return DaoFactory.serviceDao().add(s);
+    }
+
+    public List<Service> getAll() throws ServiceException {
+        return DaoFactory.serviceDao().getAll();
+    }
+
+    public Service update(Service s) throws ServiceException {
+        return DaoFactory.serviceDao().update(s);
+    }
+
+    public void delete(int id) throws ServiceException {
+        DaoFactory.serviceDao().delete(id);
+    }
 }
