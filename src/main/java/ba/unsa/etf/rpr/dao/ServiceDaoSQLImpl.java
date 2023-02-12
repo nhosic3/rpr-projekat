28f1847b585e1eac26f3bbd5569eb5dca3f04ff1
nhosic3 <nhosic3@etf.unsa.ba>
2023-02-12 13:20:21 +0100

    ServiceDaoSQLImpl updated

--- src/main/java/ba/unsa/etf/rpr/dao/ServiceDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ServiceDaoSQLImpl.java
@@ -10,7 +10,7 @@ import java.sql.Date;
 public class ServiceDaoSQLImpl extends AbstractDao<Service> implements ServiceDao {
 
     public ServiceDaoSQLImpl() {
-        super("services");
+        super("Service");
     }
 
     @Override
