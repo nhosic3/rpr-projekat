38e38ff413337d9001f617dc1098913123d82d84
nhosic3 <nhosic3@etf.unsa.ba>
2023-02-13 23:14:37 +0100

    ServiceException updated

--- src/main/java/ba/unsa/etf/rpr/exceptions/ServiceException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/ServiceException.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.exceptions;
-
+/**
+ * Custom made exception for errors
+ */
 public class ServiceException extends Exception{
     public ServiceException(String msg, Exception reason){
         super(msg, reason);
