b447869bf474a78b73d031166e3cc91f4da90d24
nhosic3 <nhosic3@etf.unsa.ba>
2023-02-13 23:16:32 +0100

    DaoFactory updated

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,13 +1,13 @@
 package ba.unsa.etf.rpr.dao;
-
+/**
+ * Implementation of factory design pattern
+ */
 public class DaoFactory {
     private static final ClientDao clientDao = new ClientDaoSQLImpl();
     private static final EmployeeDao employeeDao = new EmployeeDaoSQLImpl();
     private static final ServiceDao serviceDao = new ServiceDaoSQLImpl();
 
-    private DaoFactory(){
-
-    }
+    private DaoFactory(){}
 
     public static ClientDao clientDao(){
         return clientDao;
